42620d1cc04888c8b61af80d15c781e7c37a5fcc
mom <mgmchan@bcil.com>
2020-04-16 17:17:36 +0800

    removed /build from .gitignore

---
+++ build/precache-manifest.1167a3f5533dcc480934223a82bdf0b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a9b3a8e80cb70965f6f5b91ecddbfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35f58f5ff3ac04f01926",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36e75751e40f69941668",
+    "url": "/static/js/2.bf062c3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf062c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f58f5ff3ac04f01926",
+    "url": "/static/js/main.a443afb4.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
