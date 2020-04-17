c1ddbdb836a83d4ad9127c3bebd227bdf86057f7
mom <mgmchan@bcil.com>
2020-04-17 15:00:52 +0800

    Test full CI/CD flow

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
 					Hi There <br/> 
-					Testing  docker-compose "volumes" 
+					Testing Git-Travis-Elastic Beanstalk CI/CD
         </p>
         <a
           className="App-link"
