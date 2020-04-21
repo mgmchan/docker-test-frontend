4265c048c41ec8520f79e7e98aa1bf242702c3b1
mgmchan <mgmchan@bcil.com>
2020-04-21 23:06:36 +0800

    chcanged greeting

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-					Hi there 
+					Hi Michael 
 					<br/> 
 					Testing Git-Travis-Elastic Beanstalk CI/CD
 					<br/>
